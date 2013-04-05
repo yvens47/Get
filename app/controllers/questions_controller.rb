d99bf039c3807edb258ed45809dae22805e82a24
jean pierre <yvens47@yahoo.com>
2013-04-05 11:56:12 -0400

    add will paginate

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.paginate :page =>params[:page], :per_page =>3, :order=>"created_at desc"
 
     respond_to do |format|
       format.html # index.html.erb
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+Rails::Initializer.run do |config|
+  config.gem 'will_paginate', :version => '~> 2.3.16'
+end
 GetResult::Application.initialize!
